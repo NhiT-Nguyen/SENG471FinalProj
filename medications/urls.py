01e3cfcabff66172390a30f5e143a780f9979ce2
sherrythecrimsonberry33 <sheharyar@ucalgary.ca>
2026-04-07 21:49:37 -0600

    Refactor router implementation to use SimpleRouter for medications

--- medications/urls.py
+++ medications/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from . import views
 
-router = DefaultRouter()
-router.register(r'medications', views.MedicationViewSet)
+router = SimpleRouter()
+router.register(r'medications', views.MedicationViewSet, basename='medication')
 
 urlpatterns = [
     path('', include(router.urls)),
