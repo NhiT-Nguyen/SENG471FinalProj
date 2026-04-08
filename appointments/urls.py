3bf6fea192eb75e69c7bd209e2e54d45d498632e
sherrythecrimsonberry33 <sheharyar@ucalgary.ca>
2026-04-07 21:48:41 -0600

    Refactor router to use SimpleRouter for appointment-related endpoints

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from . import views
 
-router = DefaultRouter()
-router.register(r'appointments', views.AppointmentViewSet)
-router.register(r'availabilities', views.AvailabilityViewSet)
-router.register(r'availability-confirmations', views.AvailabilityConfirmationViewSet)
-router.register(r'appointment-requests', views.AppointmentRequestViewSet)
+router = SimpleRouter()
+router.register(r'appointments', views.AppointmentViewSet, basename='appointment')
+router.register(r'availabilities', views.AvailabilityViewSet, basename='availability')
+router.register(r'availability-confirmations', views.AvailabilityConfirmationViewSet, basename='availability-confirmation')
+router.register(r'appointment-requests', views.AppointmentRequestViewSet, basename='appointment-request')
 
 urlpatterns = [
     path('', include(router.urls)),
