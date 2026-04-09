a35a65c5897b98d3b8aa45c6e3e538e9553b062e
sherrythecrimsonberry33 <sheharyar@ucalgary.ca>
2026-04-08 19:09:56 -0600

    Enhance serializers to include additional user fields for Profile, Patient, and HealthcareProvider

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -9,18 +9,24 @@ class UserSerializer(serializers.ModelSerializer):
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    # Flat convenience fields for the frontend
+    user_username  = serializers.CharField(source='user.username', read_only=True)
+    user_email     = serializers.CharField(source='user.email', read_only=True)
+    user_first_name = serializers.CharField(source='user.first_name', read_only=True)
+    user_last_name  = serializers.CharField(source='user.last_name', read_only=True)
 
     class Meta:
         model = Profile
-        fields = ['user', 'role']
+        fields = ['user', 'role', 'user_username', 'user_email', 'user_first_name', 'user_last_name']
 
 class PatientSerializer(serializers.ModelSerializer):
     caregivers = UserSerializer(many=True, read_only=True)
     family_members = UserSerializer(many=True, read_only=True)
+    user_username = serializers.CharField(source='user.username', read_only=True)
 
     class Meta:
         model = Patient
-        fields = ['id', 'user', 'name', 'date_of_birth', 'caregivers', 'family_members']
+        fields = ['id', 'user', 'user_username', 'name', 'date_of_birth', 'caregivers', 'family_members']
 
 class PatientDetailSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
@@ -32,10 +38,16 @@ class PatientDetailSerializer(serializers.ModelSerializer):
 class HealthcareProviderSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     specialty_display = serializers.CharField(source='get_specialty_display', read_only=True)
+    user_username  = serializers.CharField(source='user.username', read_only=True)
+    user_first_name = serializers.CharField(source='user.first_name', read_only=True)
+    user_last_name  = serializers.CharField(source='user.last_name', read_only=True)
+    years_of_experience = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = HealthcareProvider
-        fields = ['id', 'user', 'specialty', 'specialty_display', 'license_number', 'hospital_clinic', 'phone_number', 'bio']
+        fields = ['id', 'user', 'user_username', 'user_first_name', 'user_last_name',
+                  'specialty', 'specialty_display', 'license_number', 'hospital_clinic',
+                  'phone_number', 'bio', 'years_of_experience']
 
 class HealthcareProviderDetailSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(source='user.get_full_name', read_only=True)
